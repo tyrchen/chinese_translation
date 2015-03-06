90f6c9bb23164a50a22538d5ed0c0fec905f763c
Tyr Chen <tyr.chen@gmail.com>
2015-03-05 20:53:08 -0800

    bump the version to 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ChineseTranslation.Mixfile do
 
   def project do
     [app: :chinese_translation,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.0",
      deps: deps]
   end
