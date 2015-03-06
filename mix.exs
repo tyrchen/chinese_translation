18560e0db1d40c81c11311c8bd0594bb3a593590
Tyr Chen <tyr.chen@gmail.com>
2015-03-05 21:41:55 -0800

    update mix.exs for hex support

--- mix.exs
+++ mix.exs
@@ -5,7 +5,10 @@ defmodule ChineseTranslation.Mixfile do
     [app: :chinese_translation,
      version: "0.1.0",
      elixir: "~> 1.0",
-     deps: deps]
+     description: description,
+     package: package,
+     deps: deps,
+   ]
   end
 
   # Configuration for the OTP application
@@ -20,5 +23,20 @@ defmodule ChineseTranslation.Mixfile do
     [
       {:httpoison, "~> 0.6"},
     ]
+  end
+
+  defp description do
+    """
+    ChineseTranslation provides traditional chinese <-> simplified chinese translation, as well as pinyin translation and slugify for chinese phrases/characters.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "data", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+      contributors: ["Tyr Chen"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/tyrchen/chinese_translation"}
+   ]
   end
 end
